773474dbbee08d6e5a6626edb3658383825d608f
Elvin Ramirez <real8686@gmail.com>
2018-02-09 23:53:27 -0500

    frat: finish Player

--- 11 Custom HTML5 Video Player/scripts.js
+++ 11 Custom HTML5 Video Player/scripts.js
@@ -8,7 +8,6 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
-
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
@@ -20,10 +19,23 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
-// update
+function skip() {
+ video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Section for hndle on update and progress
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 
 /* Hook up the event listeners */
@@ -32,5 +44,14 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
-// Add toggle clicl
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// Handler for mouse listeners
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
