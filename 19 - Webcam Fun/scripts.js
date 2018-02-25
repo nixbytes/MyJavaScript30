a7682e25f0f10d5b74c2af54638e9bc77eb68a1f
Elvin Ramirez <real8686@gmail.com>
2018-02-25 17:17:02 -0500

    add paint to canvas func

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -5,19 +5,32 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 
-function getVideo(){
-	navigator.mediaDevices.getUserMedia({video:true,audio:false})
-	.then(localMediaStream =>{
-		console.log(localMediaStream);
-		video.src = window.URL.createObjectURL(localMediaStream);
-		video.play();
-	});
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
 
-	.catch(err => {
-		console.error(`Need Access for Web Cam`,err);
-	})
+        .catch(err => {
+            console.error(`Need Access for Web Cam`, err);
+        })
+}
+
+function paintToCanavas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
 }
 
 
 
-getVideo();  
+getVideo();
