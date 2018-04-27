a878fc14d0afe09ab3c61c6b7e4873eec7fc68cc
ezwebcraft <real8686@gmail.com>
2018-04-26 22:59:21 -0400

    test now secs and then secs

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,16 @@
+
+
+function timer(seconds){
+
+const now = Date.now();
+
+const then = now + seconds * 1000;
+
+console.log({now, then});
+    // setInterval(function(){
+
+    // seconds--;
+
+    // },1000);
+
+}
