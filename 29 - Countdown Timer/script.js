d9a8d3c4c9b2f2c8a9e70cedcce3da81b558c2ca
ezwebcraft <real8686@gmail.com>
2018-04-27 21:34:38 -0400

    add clearInterval and setInternvals

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,16 +1,23 @@
+function timer(seconds) {
+
+    const now = Date.now();
+
+    const then = now + seconds * 1000;
 
 
-function timer(seconds){
+    countdown = setInterval(()=>{
+        const secondsLeft = Math.round((then - Data.now())/1000);
 
-const now = Date.now();
-
-const then = now + seconds * 1000;
-
-console.log({now, then});
-    // setInterval(function(){
-
-    // seconds--;
-
-    // },1000);
+    // check if stop it
+    if(secondsLeft <= 0){
+        clearInterval(countdown);
+        return;
+    }
+        
+    },1000);
+    console.log({
+        now,
+        then
+    });
 
 }
