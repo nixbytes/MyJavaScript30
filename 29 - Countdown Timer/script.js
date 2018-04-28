344fe1125c1e340c39911e3060b786867467cd39
ezwebcraft <real8686@gmail.com>
2018-04-28 12:17:39 -0400

    add end and hour var

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,3 +1,7 @@
+let countdown;
+
+const timerDisplay =  
+
 function timer(seconds) {
 
     const now = Date.now();
@@ -5,27 +9,44 @@ function timer(seconds) {
     const then = now + seconds * 1000;
 
 
-    countdown = setInterval(()=>{
-        const secondsLeft = Math.round((then - Date.now())/1000);
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-    // check if stop it
-    if(secondsLeft <= 0){
-        clearInterval(countdown);
-        return;
-    }
-    console.log({
-        now,
-        then
-    });
+        // check if stop it
+        if (secondsLeft <= 0) {
+            clearInterval(countdown);
+            return;
+        }
+        console.log({
+            now,
+            then
+        });
 
-        
-    },1000);
+
+    }, 1000);
 }
 
-function displayTimeLeft(second){
-const minutes = Math.floor(seconds/60);
+function displayTimeLeft(second) {
+    const minutes = Math.floor(seconds / 60);
 
-const remainderSeconds = seconds % 60;
-console.log({minutes,remainderSeconds});
+    const remainderSeconds = seconds % 60;
+    console.log({
+        minutes,
+        remainderSeconds
+    });
+
+}
+
+function displayEndTime(timestamp){
+
+    const end = end Date(timestamp);
+
+    const hour = end.getHous();
+
+    const adjustedHour = hour > 12 ? hour -12 : hour;
+
+    const minutes = end.getMintues();
+
+
 
 }
